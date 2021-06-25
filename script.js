792052c589adeda6a5d0a910f1ec88696cf637ce
Paryank0419 <sunny.namdeo14@gmail.com>
2021-06-25 20:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if(principal == "" || parseInt(principal)<=0){
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+     return;
+  }
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
