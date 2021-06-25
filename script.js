68ff3890435b80fac43d784ed6dc3bc0d0ff3977
Paryank0419 <sunny.namdeo14@gmail.com>
2021-06-25 20:48:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var interest = principal * years * rate /100;
 var amount = parseInt(principal) + parseInt(interest);
 var year = new Date().getFullYear()+parseInt(years);
 
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+document.getElementById("result").innerHTML="If you deposit <span class='highlight'>"+principal+"</span>.,\<br\>at an interest rate of <span class='highlight'>"+rate+"%</span>.\<br\>You will receive an amount of <span class='highlight'>"+amount+"</span>,\<br\>in the year <span class='highlight'>"+year+"</span>.\<br\>"
     
 }
 function updateRate() 
